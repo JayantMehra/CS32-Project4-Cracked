a396c10d01cb06ea7ceb26677d2a394bf1f626d6
Jayant Mehra <mehrajayant@gmail.com>
2018-03-10 19:15:02 -0800

    Initial Commit

---
+++ Project 4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project 4
+//
+//  Created by Jayant Mehra on 3/10/18.
+//  Copyright © 2018 Jayant Mehra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
